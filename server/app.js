9a6902d77130e4cb77c3ab23829b5fe3f2b6cb79
DuncanMcLean <42756134+DuncanMcLean@users.noreply.github.com>
2019-12-19 14:13:58 -0500

    Initial Electron setup

---
+++ server/app.js
@@ -0,0 +1,38 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let mainWindow
+
+
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  // load the index.html of the app.
+  mainWindow.loadFile('client/views/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// Creates the window after Electron has initialized 
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
